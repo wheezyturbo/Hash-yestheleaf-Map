09d28a88ea975ea854876459e5c0787bea480b59
rubyCiide <joelpaul900@gmail.com>
2024-02-27 13:54:42 +0530

    Add hash function

--- hash_map.js
+++ hash_map.js
@@ -2,6 +2,16 @@ class HashMap {
    constructor(){
     this.buckets = new Array(100);
    }
+  _hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode % this.buckets.length;
+  }
 }
 
 module.exports = HashMap;
